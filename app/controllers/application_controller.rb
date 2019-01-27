014bdbdeadc4fcd0e598ca506948906d0f337c39
Nick Inman <nick@theoatmeal.com>
2019-01-27 20:20:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
     if params[:password] == ""
+      binding.pry
       redriect '/failure'
     end
     if user && user.authenticate(params[:password])
