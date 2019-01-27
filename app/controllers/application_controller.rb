225384b2abb3d53210d9b7230bf50902166f7ae1
Nick Inman <nick@theoatmeal.com>
2019-01-27 20:22:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
+      binding.pry
       session[:user_id] = user.id
       redirect '/account'
     else
